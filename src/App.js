5560f49c44740a53fa6f5d24c973f68ab6926109
rht16 <rohitkrrt@gmail.com>
2021-11-02 23:29:39 +0530

    sign in and sign page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
+import {Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+ import Signup from './pages/Signup';
+import Container from './pages/Signin'
 function App() {
   return (
     <div className="App">
+      {/* <Signup /> */}
+      <Container />
+      {/* <Router>
+      <Switch>
+
+        <Route path="/page3">
       
+        <Container/>
+        </Route>
+
+        <Route path='/'>
+        <Container />
+        </Route>
+     
+      <Login />
+      </Switch>
+      </Router> */}
     </div>
   );
 }
