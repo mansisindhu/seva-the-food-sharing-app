6e562e4b9c3b7da9a41e25d8e73aeb76cdff8937
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-09 13:36:09 +0530

    Ngo cards added on home screen

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
 import styles from "./homePage.module.css";
-import { BiArrowToRight, BiSearch } from "react-icons/bi";
+import { BiSearch } from "react-icons/bi";
 import { RiArrowRightSLine } from "react-icons/ri";
+
+import NGOCard from "../../components/NGOCard";
+
 const HomePage = (props) => {
   const { data } = props;
-  console.log(data)
+  
   return (
     <div className={styles.main}>
       <div className={styles.main_top}>
@@ -27,9 +30,28 @@ const HomePage = (props) => {
         </div>
         <div className={styles.round_images}>
           {data.map((el) => {
-            return (<img key={el.reviews} className={styles.round_image} src={el.image} alt="" />)
+            return (
+              <img
+                key={el.reviews}
+                className={styles.round_image}
+                src={el.image}
+                alt=""
+              />
+            );
           })}
         </div>
+      </div>
+      <div className={styles.food_required_section}>
+        <div className={styles.food_required_top}>
+          <h3>Food Required</h3>
+          <div className={styles.see_all}>
+            <p>See all</p>
+            <RiArrowRightSLine className={styles.search_icon} />
+          </div>
+        </div>
+        <NGOCard data={data[0]}/>
+        <NGOCard data={data[1]}/>
+        <NGOCard data={data[4]}/>
       </div>
     </div>
   );
