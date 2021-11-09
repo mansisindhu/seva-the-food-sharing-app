cdd74ba80bd5bf3ea6265f11317b9aa7851efeea
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-09 23:33:29 +0530

    Ngo page added

---
+++ src/pages/NGOPage/NGOPage.jsx
@@ -0,0 +1,67 @@
+import styles from "./ngoPage.module.css";
+
+import { HiBadgeCheck } from "react-icons/hi";
+
+import BottomNavbar from "../../components/BottomNavbar";
+import DonateFoodNavbar from "../../components/DonateFoodNavbar";
+import Button from "../../components/Button";
+
+const NGOPage = (props) => {
+  const { ngoData } = props;
+
+  return (
+    <>
+      <DonateFoodNavbar />
+      <BottomNavbar />
+      <div className={styles.main}>
+        <div className={styles.ngo_details}>
+          <div className={styles.image_section}>
+            <img src={ngoData.image} alt="ngo image" />
+            <div className={styles.title}>
+              <p>{ngoData.NGOName}</p>
+              <HiBadgeCheck className={styles.icon} />
+            </div>
+          </div>
+          <div className={styles.grid}>
+            <div className={styles.item}>
+              <p className={styles.detail}>{ngoData.reviews}</p>
+              <p className={styles.para}>Reviews</p>
+            </div>
+            <div className={styles.item}>
+              <p className={styles.detail}>{ngoData.totalFeeds}+</p>
+              <p className={styles.para}>Total Feeds</p>
+            </div>
+            <div className={styles.item}>
+              <p className={styles.detail}>{ngoData.totalCampaigns}+</p>
+              <p className={styles.para}>Total Campaigns</p>
+            </div>
+            <div className={styles.item}>
+              <p className={styles.detail}>{ngoData.totalVolunteers}+</p>
+              <p className={styles.para}>Total Volunteers</p>
+            </div>
+          </div>
+          <div className={styles.button}>
+            <Button text="Donate Now" />
+          </div>
+        </div>
+
+        <div className={styles.about}>
+          <div className={styles.about_top}>
+            <p>About</p>
+            <p>Events</p>
+            <p>Reviews</p>
+          </div>
+          <div className={styles.about_bottom}>
+            <p>{ngoData.about}</p>
+            <div className={styles.buttons}>
+              <button>Chat</button>
+              <button>Volunteer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NGOPage;
