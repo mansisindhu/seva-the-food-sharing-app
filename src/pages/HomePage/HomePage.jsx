c306b539df7ca7df03efe31cb7db7847962825e4
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-09 18:21:18 +0530

    Upcoming campaigns section done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,18 @@ import { RiArrowRightSLine } from "react-icons/ri";
 
 import NGOCard from "../../components/NGOCard";
 
+const campaignImages = [
+  "https://thumbs.dreamstime.com/z/meal-food-donation-app-smartphone-volunteering-charity-concept-covid-solidarity-response-181880570.jpg",
+  "https://media.istockphoto.com/vectors/food-donation-and-charity-vector-id1224414210?k=20&m=1224414210&s=612x612&w=0&h=FhZYeea62Eh_7OM74djnSdkRBSq0kpeloV3SnyTiSpE=",
+  "https://i.pinimg.com/originals/5d/83/88/5d8388343a60c402a84a687e8ad44eae.jpg",
+  "https://www.wfp.org/sites/default/files/images/news/StopTheWaste%20Press%20Release%20Image.png",
+  "https://previews.123rf.com/images/arrow/arrow1712/arrow171200010/91915955-food-drive-donation-give-today-campaign.jpg",
+  "https://c8.alamy.com/comp/2BM08C6/meal-and-food-donation-app-on-a-smartphone-volunteering-and-charity-concept-2BM08C6.jpg",
+];
+
 const HomePage = (props) => {
   const { data } = props;
-  
+
   return (
     <div className={styles.main}>
       <div className={styles.main_top}>
@@ -49,9 +58,30 @@ const HomePage = (props) => {
             <RiArrowRightSLine className={styles.search_icon} />
           </div>
         </div>
-        <NGOCard data={data[0]}/>
-        <NGOCard data={data[1]}/>
-        <NGOCard data={data[4]}/>
+        <NGOCard data={data[0]} />
+        <NGOCard data={data[1]} />
+        <NGOCard data={data[4]} />
+      </div>
+      <div className={styles.upcoming_campaigns}>
+        <div className={styles.top}>
+          <h3>Upcoming Campaigns</h3>
+          <div className={styles.see_all}>
+            <p>See all</p>
+            <RiArrowRightSLine className={styles.search_icon} />
+          </div>
+        </div>
+        <div className={styles.round_images}>
+          {campaignImages.map((el) => {
+            return (
+              <img
+                key={el}
+                className={styles.campaign_image}
+                src={el}
+                alt="campaign_img"
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
