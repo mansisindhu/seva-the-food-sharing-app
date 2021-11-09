2bb7b889f80aa6a8859ad795ba82a6dd5257062a
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-09 19:09:14 +0530

    Bottom navbar added and margin bottom increased

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { BiSearch } from "react-icons/bi";
 import { RiArrowRightSLine } from "react-icons/ri";
 
 import NGOCard from "../../components/NGOCard";
+import BottomNavbar from "../../components/BottomNavbar";
 
 const campaignImages = [
   "https://thumbs.dreamstime.com/z/meal-food-donation-app-smartphone-volunteering-charity-concept-covid-solidarity-response-181880570.jpg",
@@ -17,87 +18,90 @@ const HomePage = (props) => {
   const { data } = props;
 
   return (
-    <div className={styles.main}>
-      <div className={styles.main_top}>
-        <div className={styles.search_input}>
-          <BiSearch className={styles.search_icon} />
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="Search for NGO or campaign"
-          />
+    <>
+      <BottomNavbar />
+      <div className={styles.main}>
+        <div className={styles.main_top}>
+          <div className={styles.search_input}>
+            <BiSearch className={styles.search_icon} />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Search for NGO or campaign"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className={styles.volunteer_images}>
-        <div className={styles.top}>
-          <h3>Volunteer Required</h3>
-          <div className={styles.see_all}>
-            <p>See all</p>
-            <RiArrowRightSLine className={styles.search_icon} />
+        <div className={styles.volunteer_images}>
+          <div className={styles.top}>
+            <h3>Volunteer Required</h3>
+            <div className={styles.see_all}>
+              <p>See all</p>
+              <RiArrowRightSLine className={styles.search_icon} />
+            </div>
+          </div>
+          <div className={styles.round_images}>
+            {data.map((el) => {
+              return (
+                <img
+                  key={el.reviews}
+                  className={styles.round_image}
+                  src={el.image}
+                  alt=""
+                />
+              );
+            })}
           </div>
         </div>
-        <div className={styles.round_images}>
-          {data.map((el) => {
-            return (
-              <img
-                key={el.reviews}
-                className={styles.round_image}
-                src={el.image}
-                alt=""
-              />
-            );
-          })}
+        <div className={styles.food_required_section}>
+          <div className={styles.food_required_top}>
+            <h3>Food Required</h3>
+            <div className={styles.see_all}>
+              <p>See all</p>
+              <RiArrowRightSLine className={styles.search_icon} />
+            </div>
+          </div>
+          <NGOCard data={data[0]} />
+          <NGOCard data={data[1]} />
+          <NGOCard data={data[4]} />
         </div>
-      </div>
-      <div className={styles.food_required_section}>
-        <div className={styles.food_required_top}>
-          <h3>Food Required</h3>
-          <div className={styles.see_all}>
-            <p>See all</p>
-            <RiArrowRightSLine className={styles.search_icon} />
+        <div className={styles.upcoming_campaigns}>
+          <div className={styles.top}>
+            <h3>Upcoming Campaigns</h3>
+            <div className={styles.see_all}>
+              <p>See all</p>
+              <RiArrowRightSLine className={styles.search_icon} />
+            </div>
+          </div>
+          <div className={styles.round_images}>
+            {campaignImages.map((el) => {
+              return (
+                <img
+                  key={el}
+                  className={styles.campaign_image}
+                  src={el}
+                  alt="campaign_img"
+                />
+              );
+            })}
           </div>
         </div>
-        <NGOCard data={data[0]} />
-        <NGOCard data={data[1]} />
-        <NGOCard data={data[4]} />
-      </div>
-      <div className={styles.upcoming_campaigns}>
-        <div className={styles.top}>
-          <h3>Upcoming Campaigns</h3>
-          <div className={styles.see_all}>
-            <p>See all</p>
-            <RiArrowRightSLine className={styles.search_icon} />
-          </div>
-        </div>
-        <div className={styles.round_images}>
-          {campaignImages.map((el) => {
-            return (
-              <img
-                key={el}
-                className={styles.campaign_image}
-                src={el}
-                alt="campaign_img"
-              />
-            );
-          })}
-        </div>
-      </div>
 
-      <div className={styles.food_required_section}>
-        <div className={styles.food_required_top}>
-          <h3>Nearby NGO</h3>
-          <div className={styles.see_all}>
-            <p>See all</p>
-            <RiArrowRightSLine className={styles.search_icon} />
+        <div className={styles.food_required_section}>
+          <div className={styles.food_required_top}>
+            <h3>Nearby NGO</h3>
+            <div className={styles.see_all}>
+              <p>See all</p>
+              <RiArrowRightSLine className={styles.search_icon} />
+            </div>
+          </div>
+          <div className={styles.nearby_images}>
+            <img className={styles.nearby_image} src={data[0].image} alt="" />
+            <img className={styles.nearby_image} src={data[1].image} alt="" />
           </div>
         </div>
-        <div className={styles.nearby_images}>
-          <img className={styles.nearby_image} src={data[0].image} alt="" />
-          <img className={styles.nearby_image} src={data[1].image} alt="" />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
