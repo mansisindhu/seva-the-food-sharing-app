1598d6a63c34107f55db91a7bb541c53a7ff5810
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-09 10:55:07 +0530

    Added volunteers section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,35 @@
 import styles from "./homePage.module.css";
-import { BiHomeAlt, BiSearch } from "react-icons/bi";
-
-const HomePage = () => {
+import { BiArrowToRight, BiSearch } from "react-icons/bi";
+import { RiArrowRightSLine } from "react-icons/ri";
+const HomePage = (props) => {
+  const { data } = props;
+  console.log(data)
   return (
     <div className={styles.main}>
-      <div className={styles.search_input}>
-        <BiSearch className={styles.search_icon} />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Search for NGO or campaign"
-        />
+      <div className={styles.main_top}>
+        <div className={styles.search_input}>
+          <BiSearch className={styles.search_icon} />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Search for NGO or campaign"
+          />
+        </div>
+      </div>
+
+      <div className={styles.volunteer_images}>
+        <div className={styles.top}>
+          <h3>Volunteer Required</h3>
+          <div className={styles.see_all}>
+            <p>See all</p>
+            <RiArrowRightSLine className={styles.search_icon} />
+          </div>
+        </div>
+        <div className={styles.round_images}>
+          {data.map((el) => {
+            return (<img key={el.reviews} className={styles.round_image} src={el.image} alt="" />)
+          })}
+        </div>
       </div>
     </div>
   );
