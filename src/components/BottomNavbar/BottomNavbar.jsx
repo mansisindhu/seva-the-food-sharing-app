da74c993e1358d33943ee52b5def4170c8e3a5ee
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-11 00:07:23 +0530

    Added links to bottom navbar

--- src/components/BottomNavbar/BottomNavbar.jsx
+++ src/components/BottomNavbar/BottomNavbar.jsx
@@ -4,30 +4,39 @@ import { GrLocation } from "react-icons/gr";
 import { RiTimeLine } from "react-icons/ri";
 
 import styles from "./bottomNavbar.module.css";
+import { Link } from "react-router-dom";
 
 const BottomNavbar = () => {
   return (
     <div className={styles.navbar}>
-      <div className={styles.link}>
-        <BiHomeAlt className={styles.icon} />
-        <p className={styles.link_text}>Home</p>
-      </div>
-      <div className={styles.link}>
-        <RiTimeLine className={styles.icon} />
-        <p className={styles.link_text}>Activity</p>
-      </div>
-      <div className={styles.link}>
-        <BiDonateHeart className={styles.icon} />
-        <p className={styles.link_text}>Donate</p>
-      </div>
-      <div className={styles.link}>
+      <Link to="/">
+        <div className={styles.link}>
+          <BiHomeAlt className={styles.icon} />
+          <p className={styles.link_text}>Home</p>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={styles.link}>
+          <RiTimeLine className={styles.icon} />
+          <p className={styles.link_text}>Activity</p>
+        </div>
+      </Link>
+      <Link to="/donationType">
+        <div className={styles.link}>
+          <BiDonateHeart className={styles.icon} />
+          <p className={styles.link_text}>Donate</p>
+        </div>
+      </Link>
+      <div className={[styles.link, styles.disable].join(" ")}>
         <GrLocation className={styles.icon} />
         <p className={styles.link_text}>Hunger Spot</p>
       </div>
-      <div className={styles.link}>
-        <CgProfile className={styles.icon} />
-        <p className={styles.link_text}>Profile</p>
-      </div>
+      <Link to="/profile">
+        <div className={styles.link}>
+          <CgProfile className={styles.icon} />
+          <p className={styles.link_text}>Profile</p>
+        </div>
+      </Link>
     </div>
   );
 };
