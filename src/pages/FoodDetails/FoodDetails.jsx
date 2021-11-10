cef39bca54d7a9b9fd332db328f3248458930141
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-10 14:18:17 +0530

    Added range slider in food donation

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -3,6 +3,8 @@ import styles from "./foodDetails.module.css";
 import BottomNavbar from "../../components/BottomNavbar";
 import DonateFoodNavbar from "../../components/DonateFoodNavbar";
 
+import Button from "../../components/Button";
+
 const FoodDetails = () => {
   return (
     <>
@@ -47,9 +49,35 @@ const FoodDetails = () => {
         </div>
 
         <div className={styles.range_slider}>
-          <p className={styles.heading}>Meal type</p>
+          <p className={styles.heading}>Quantity (person)</p>
+          <input type="range" className={styles.slider} min="0" max="60" />
+          <div className={styles.numbers}>
+            <p>0</p>
+            <p>10</p>
+            <p>20</p>
+            <p>30</p>
+            <p>40</p>
+            <p>50</p>
+            <p>60</p>
+          </div>
+        </div>
 
-          <input type="range" className={styles.slider} min="1" max="60" />
+        <div className={styles.range_slider}>
+          <p className={styles.heading}>When was the meal prepared (Hrs)</p>
+          <input type="range" className={styles.slider} min="0" max="12" />
+          <div className={styles.numbers}>
+            <p>0</p>
+            <p>2</p>
+            <p>4</p>
+            <p>6</p>
+            <p>8</p>
+            <p>10</p>
+            <p>12</p>
+          </div>
+        </div>
+
+        <div className={styles.btn}>
+          <Button text="Post" />
         </div>
       </div>
     </>
