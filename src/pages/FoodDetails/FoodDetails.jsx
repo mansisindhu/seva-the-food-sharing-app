9f1bf342fc4a1f644cda9bfc7bb98d1c66f15fb6
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-10 13:55:28 +0530

    Added checkboxes and radio buttons

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -9,16 +9,48 @@ const FoodDetails = () => {
       <BottomNavbar />
       <DonateFoodNavbar />
       <div className={styles.main}>
-        <input type="range" className={styles.slider} min="1" max="100" />
+        <p className={styles.heading}>Meal type</p>
+        <div className={styles.radios}>
+          <p>
+            <input type="radio" id="test1" name="radio-group" checked />
+            <label className={styles.label} for="test1">
+              Veg
+            </label>
+          </p>
+          <p>
+            <input type="radio" id="test2" name="radio-group" />
+            <label className={styles.label} for="test2">
+              Non-veg
+            </label>
+          </p>
+        </div>
 
-        <p>
-          <input type="radio" id="test1" name="radio-group" checked />
-          <label for="test1">Apple</label>
-        </p>
-        <p>
-          <input type="radio" id="test2" name="radio-group" />
-          <label for="test2">Peach</label>
-        </p>
+        <div className={styles.checkboxes}>
+          <div className={styles.checkbox_container}>
+            <input type="checkbox" className={styles.checkbox} />
+            <img className={styles.overlay} src="./images/breakfast.jpg" />
+            <div className={styles.checked_overlay}></div>
+            <p>Breakfast</p>
+          </div>
+          <div className={styles.checkbox_container}>
+            <input type="checkbox" className={styles.checkbox} />
+            <img className={styles.overlay} src="./images/lunch.jpg" />
+            <div className={styles.checked_overlay}></div>
+            <p>Lunch</p>
+          </div>
+          <div className={styles.checkbox_container}>
+            <input type="checkbox" className={styles.checkbox} />
+            <img className={styles.overlay} src="./images/dinner.jpg" />
+            <div className={styles.checked_overlay}></div>
+            <p>Dinner</p>
+          </div>
+        </div>
+
+        <div className={styles.range_slider}>
+          <p className={styles.heading}>Meal type</p>
+
+          <input type="range" className={styles.slider} min="1" max="60" />
+        </div>
       </div>
     </>
   );
