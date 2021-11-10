bfd529556bb1bd72344e9eef8af6a26c577567eb
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-10 22:00:21 +0530

    State uplifting for form data

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -4,23 +4,41 @@ import BottomNavbar from "../../components/BottomNavbar";
 import DonateFoodNavbar from "../../components/DonateFoodNavbar";
 
 import Button from "../../components/Button";
+import { useState } from "react";
 
-const FoodDetails = () => {
+const FoodDetails = (props) => {
+  const { handleInput } = props;
+
+  const [quanity, setQuantity] = useState(0);
+  const [time, setTime] = useState(0);
+
   return (
     <>
       <BottomNavbar />
-      <DonateFoodNavbar link="/category"/>
+      <DonateFoodNavbar link="/category" />
       <div className={styles.main}>
         <p className={styles.heading}>Meal type</p>
         <div className={styles.radios}>
           <p>
-            <input type="radio" id="test1" name="radio-group" checked />
+            <input
+              onClick={handleInput}
+              value="Veg"
+              type="radio"
+              id="test1"
+              name="type"
+            />
             <label className={styles.label} for="test1">
               Veg
             </label>
           </p>
           <p>
-            <input type="radio" id="test2" name="radio-group" />
+            <input
+              onClick={handleInput}
+              value="Non-veg"
+              type="radio"
+              id="test2"
+              name="type"
+            />
             <label className={styles.label} for="test2">
               Non-veg
             </label>
@@ -29,19 +47,37 @@ const FoodDetails = () => {
 
         <div className={styles.checkboxes}>
           <div className={styles.checkbox_container}>
-            <input type="checkbox" className={styles.checkbox} />
+            <input
+              onClick={handleInput}
+              name="meal"
+              value="Breakfast"
+              type="checkbox"
+              className={styles.checkbox}
+            />
             <img className={styles.overlay} src="./images/breakfast.jpg" />
             <div className={styles.checked_overlay}></div>
             <p>Breakfast</p>
           </div>
           <div className={styles.checkbox_container}>
-            <input type="checkbox" className={styles.checkbox} />
+            <input
+              onClick={handleInput}
+              name="meal"
+              type="checkbox"
+              value="Lunch"
+              className={styles.checkbox}
+            />
             <img className={styles.overlay} src="./images/lunch.jpg" />
             <div className={styles.checked_overlay}></div>
             <p>Lunch</p>
           </div>
           <div className={styles.checkbox_container}>
-            <input type="checkbox" className={styles.checkbox} />
+            <input
+              onClick={handleInput}
+              name="meal"
+              type="checkbox"
+              value="Dinner"
+              className={styles.checkbox}
+            />
             <img className={styles.overlay} src="./images/dinner.jpg" />
             <div className={styles.checked_overlay}></div>
             <p>Dinner</p>
@@ -50,7 +86,16 @@ const FoodDetails = () => {
 
         <div className={styles.range_slider}>
           <p className={styles.heading}>Quantity (person)</p>
-          <input type="range" className={styles.slider} min="0" max="60" />
+          <input
+            value={quanity}
+            onChange={(e) => setQuantity(e.value)}
+            onClick={handleInput}
+            name="quantity"
+            type="range"
+            className={styles.slider}
+            min="0"
+            max="60"
+          />
           <div className={styles.numbers}>
             <p>0</p>
             <p>10</p>
@@ -64,7 +109,14 @@ const FoodDetails = () => {
 
         <div className={styles.range_slider}>
           <p className={styles.heading}>When was the meal prepared (Hrs)</p>
-          <input type="range" className={styles.slider} min="0" max="12" />
+          <input
+            value={time}
+            onChange={(e) => setTime(e.value)}
+            type="range"
+            className={styles.slider}
+            min="0"
+            max="12"
+          />
           <div className={styles.numbers}>
             <p>0</p>
             <p>2</p>
@@ -77,7 +129,7 @@ const FoodDetails = () => {
         </div>
 
         <div className={styles.btn}>
-          <Button text="Post" />
+          <Button text="Post" link="/confirmFoodDetails" />
         </div>
       </div>
     </>
